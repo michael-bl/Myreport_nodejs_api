0c73b292ae0d53e5a3e12acf6fd861a115817558
michael-bl <61284039+michael-bl@users.noreply.github.com>
2022-09-28 22:58:44 -0600

    Actualización básica de prueba
    Se cambia la ruta de acceso raíz hacia heroku en lugar de localhost.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,41 +1,41 @@
 
-$("#add_bottleneck").submit(function(event){
+$("#add_bottleneck").submit(function (event) {
     alert("Data inserted seccessfully!");
 })
 
-$("#update_bottleneck").submit(function(event){
+$("#update_bottleneck").submit(function (event) {
     event.preventDefault();
 
     var unindexed_array = $(this).serializeArray();
     var data = {}
-    $.map(unindexed_array,function(n, i) {
+    $.map(unindexed_array, function (n, i) {
         data[n['name']] = n['value']
     })
     console.log(data);
 
     var request = {
-        "url":`http://localhost:3000/api/bottleneck/${data.code}`,
-        "method":"PUT",
-        "data":data
+        "url": `http://localhost:3000/api/bottleneck/${data.code}`,
+        "method": "PUT",
+        "data": data
     }
 
-    $.ajax(request).done(function(response){
+    $.ajax(request).done(function (response) {
         alert("Report updated successfully!");
         location.reload()
-    })    
+    })
 })
 
-if(window.location.pathname=="/"){
+if (window.location.pathname == "/") {
     $ondelete = $(".table tbody td a.delete");
-    $ondelete.click(function(){
+    $ondelete.click(function () {
         var code = $(this).attr("data-code")
 
         var request = {
-            "url":`http://localhost:3000/api/bottleneck/${code}`,
-            "method":"DELETE"
+            "url": `http://localhost:3000/api/bottleneck/${code}`,
+            "method": "DELETE"
         }
-        if(confirm("Do you really want to delete this record?")){
-            $.ajax(request).done(function(response){
+        if (confirm("Do you really want to delete this record?")) {
+            $.ajax(request).done(function (response) {
                 alert("Report deleted successfully!");
                 location.reload()
             })
--- server/services/render.js
+++ server/services/render.js
@@ -1,27 +1,27 @@
 const axios = require('axios');
 
-exports.homeRoutes = (req, res) =>{
+exports.homeRoutes = (req, res) => {
     // Making a get request to /api/bottleneck
-    //axios.get('https://myreport-api-nodejs.herokuapp.com/api/bottleneck')
-    axios.get('http://localhost:3000/api/bottleneck')    
-    .then(function(response){
-        res.render('index', {bottlenecks:response.data});
-    })
-    .catch(err=>{
-        res.send(err);
-    })   
+    //axios.get('http://localhost:3000/api/bottleneck')
+    axios.get('https://myreport-api-nodejs.herokuapp.com/api/bottleneck')
+        .then(function (response) {
+            res.render('index', { bottlenecks: response.data });
+        })
+        .catch(err => {
+            res.send(err);
+        })
 }
 
-exports.add_bottleneck = (req, res) =>{
+exports.add_bottleneck = (req, res) => {
     res.render('add_bottleneck');
 }
 
-exports.update_bottleneck = (req, res) =>{
-    axios.get('http://localhost:3000/api/bottleneck',{params:{code:req.query.code}})
-    .then(function(bottleneckdata){
-        res.render("update_bottleneck", {bottleneck:bottleneckdata.data})
-    })
-    .catch(err=>{
-        res.send(err);
-    })
+exports.update_bottleneck = (req, res) => {
+    axios.get('http://localhost:3000/api/bottleneck', { params: { code: req.query.code } })
+        .then(function (bottleneckdata) {
+            res.render("update_bottleneck", { bottleneck: bottleneckdata.data })
+        })
+        .catch(err => {
+            res.send(err);
+        })
 }
